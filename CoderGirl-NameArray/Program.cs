8662dbc3da7c44edce58c226c4f6f60cb406b639
msunkist99 <msunkist99@gmail.com>
2019-02-20 18:40:35 -0600

    Add project files.

---
+++ CoderGirl-NameArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoderGirl_NameArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
